5509fd67ceba125df170b5ec66d1025d07805e94
wookyoungK <pd1125@naver.com>
2022-05-27 14:46:02 +0900

    thymeleaf template엔진

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -6,10 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-
+    // get 방식의 method
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
+        /*
+        * data는 model이라는 것을 통해 넘기면서
+        * resources의 return의 hello와 같은 파일 즉, resources에서 hello라는 곳에 넘긴다.
+        * 컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버( viewResolver )가 화면을 찾아서 처리한다.
+        * 스프링 부트 템플릿엔진 기본 viewName 매핑
+        * resources:templates/ +{ViewName}+ .html
+        * */
         return "hello";
     }
 }
