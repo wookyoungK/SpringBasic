f166bea16d4583080121f313a0e4868b8881f1eb
wookyoungK <pd1125@naver.com>
2022-05-30 23:54:26 +0900

    간단한 회원 도메인 및 인터페이스 설계

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,41 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    // 실무에서는 동시성 문제가 있어서 해당 변수들을 다른방법으로 고려해봐야한다.
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+     /*
+     * interface에 있는 method? 오버라이드
+      */
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        //null 체크
+        return Optional.ofNullable(fstore.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
