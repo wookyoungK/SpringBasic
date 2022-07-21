7520d494d5c170382045b9509b3d0e11bbbda908
wookyoungK <pd1125@naver.com>
2022-07-21 23:17:01 +0900

    메인 메서드 테스트-비추천

--- spring-core/src/main/java/hello/core/OrderApp.java
+++ spring-core/src/main/java/hello/core/OrderApp.java
@@ -4,17 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceimpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
-public class MemberApp {
+public class OrderApp {
     public static void main(String[] args) {
         //순수 자바 테스트
         //메인메서드에서 테스트 하는 것은 안좋다
         MemberService memberService = new MemberServiceimpl();
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId,"itemA", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1L);
-        System.out.println("member = " + member.getName());
-        System.out.println("find Member = " + findMember.getName());
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order.toString());
     }
 }
