aa004763ed9f31c707f6ae77c87c6b573626b2b0
wookyoungK <pd1125@naver.com>
2022-07-28 22:36:27 +0900

    DIP 위반

--- spring-core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,8 +8,14 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 인터페이스에만 의존하도록 변경
+    private DiscountPolicy discountPolicy;
 
+    /*
+    DIP 위반
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    */
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
