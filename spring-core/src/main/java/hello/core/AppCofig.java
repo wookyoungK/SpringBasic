8eec5f50f7066d347f3c38951dff8030dd286d4f
wookyoungK <pd1125@naver.com>
2022-07-30 23:35:59 +0900

    기능 변경 테스트

--- spring-core/src/main/java/hello/core/AppCofig.java
+++ spring-core/src/main/java/hello/core/AppCofig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -26,6 +27,7 @@ public class AppCofig  {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
