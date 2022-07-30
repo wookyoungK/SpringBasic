b9c8db89eefa9c8cb915ada6b62571020fe78b55
wookyoungK <pd1125@naver.com>
2022-07-30 23:26:25 +0900

    AppConfig Refactoring - 역할,구분

--- spring-core/src/main/java/hello/core/AppCofig.java
+++ spring-core/src/main/java/hello/core/AppCofig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +12,20 @@ import hello.core.order.OrderServiceImpl;
 public class AppCofig  {
     // 구현 객체를 생성
     // 생성자를 통해서 주입
+
+    //역할, 구현을 구분 했지만 어떻게 구현했는지 한 눈에 보여야 한다.
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
