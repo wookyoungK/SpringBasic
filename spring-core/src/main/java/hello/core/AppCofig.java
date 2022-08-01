673a602550fb61e7fec178e4601302aa22adce52
wookyoungK <pd1125@naver.com>
2022-08-01 23:33:13 +0900

    스프링 컨테이너 - 스프링 빈

--- spring-core/src/main/java/hello/core/AppCofig.java
+++ spring-core/src/main/java/hello/core/AppCofig.java
@@ -9,23 +9,36 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppCofig  {
-    // 구현 객체를 생성
-    // 생성자를 통해서 주입
+/*     구현 객체를 생성
+     생성자를 통해서 주입
 
-    //역할, 구현을 구분 했지만 어떻게 구현했는지 한 눈에 보여야 한다.
+    역할, 구현을 구분 했지만 어떻게 구현했는지 한 눈에 보여야 한다.
+
+* Spring Start
+* @Bean : 스프링 컨테이너에 객체를 스프링 빈으로 등록
+*
+* */
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- spring-core/src/main/java/hello/core/MemberApp.java
+++ spring-core/src/main/java/hello/core/MemberApp.java
@@ -4,15 +4,24 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        //메인메서드에서 테스트 하는 것은 안좋다
-        //순수 자바 테스트
-        //MemberService memberService = new MemberServiceImpl();
-        AppCofig appCofig = new AppCofig();
-        MemberService memberService = appCofig.memberService();
+/*        메인메서드에서 테스트 하는 것은 안좋다
+        순수 자바 테스트
+        MemberService memberService = new MemberServiceImpl();
+*/
+
+//        AppCofig appCofig = new AppCofig();
+//        MemberService memberService = appCofig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppCofig.class);
+        //스프링 컨테이너에서 꺼내는것
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
