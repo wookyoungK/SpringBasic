ddbdf8367c9e4273cf3cc55830e0250d515efb84
wookyoungK <pd1125@naver.com>
2022-08-01 23:33:42 +0900

    스프링 컨테이너 - 스프링 빈

--- spring-core/src/main/java/hello/core/OrderApp.java
+++ spring-core/src/main/java/hello/core/OrderApp.java
@@ -7,16 +7,23 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        //순수 자바 테스트
-        //메인메서드에서 테스트 하는 것은 안좋다
-        //MemberService memberService = new MemberServiceImpl();
-        //OrderService orderService = new OrderServiceImpl();
-        AppCofig appCofig = new AppCofig();
-        MemberService memberService = appCofig.memberService();
-        OrderService orderService = appCofig.orderService();
+/*        순수 자바 테스트
+        메인메서드에서 테스트 하는 것은 안좋다
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+*/
+//        AppCofig appCofig = new AppCofig();
+//        MemberService memberService = appCofig.memberService();
+//        OrderService orderService = appCofig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppCofig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId,"itemA", Grade.VIP);
