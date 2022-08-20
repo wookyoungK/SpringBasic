0280d5ae0d8b95f0f1a87f0e86b9f4b790dcac61
wookyoungK <pd1125@naver.com>
2022-08-20 12:48:49 +0900

    탐색 위치 및 기본 스캔 대상

--- spring-core/src/main/java/hello/core/AutoAppConfig.java
+++ spring-core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,24 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 //componentScan 에서 제외할 것들을 지정해주는 것
+/*
+        컴포넌트 스캔은 @Component 뿐만 아니라 다음과 내용도 추가로 대상에 포함한다.
+        @Component : 컴포넌트 스캔에서 사용
+        @Controlller : 스프링 MVC 컨트롤러에서 사용
+        @Service : 스프링 비즈니스 로직에서 사용
+        @Repository : 스프링 데이터 접근 계층에서 사용
+        @Configuration : 스프링 설정 정보에서 사용
+*/
 @ComponentScan(
+/*        탐색할 패키지의 시작 위치를 정한다. 이 패키지를 포함해서 하위패키지에서 찾는다.
+        basePackages = "hello.core.member",
+        basePackageClasses -> 지정한 클래스의 패키지를 탐색 시작 위치로 지정한다
+
+        권장하는 방법
+        개인적으로 즐겨 사용하는 방법은 패키지 위치를 지정하지 않고, 설정 정보 클래스의 위치를 프로젝트
+        최상단에 두는 것이다. 최근 스프링 부트도 이 방법을 기본으로 제공한다.
+*/
+
         //예제 유지를 위해 제외시킨 것 -> @Configuration이 붙은 설정 정보도 자동으로 등록되기 때문에
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
--- spring-core/src/main/java/hello/core/CoreApplication.java
+++ spring-core/src/main/java/hello/core/CoreApplication.java
@@ -3,6 +3,8 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//스프링 부트의 대표 시작 정보
+//이 설정안에 @ComponentScan 이 들어있다
 @SpringBootApplication
 public class CoreApplication {
 
