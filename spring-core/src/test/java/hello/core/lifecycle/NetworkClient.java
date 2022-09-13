c97a75c839b95d7f28811a9ff3da577de0f931a2
wookyoungK <pd1125@naver.com>
2022-09-13 22:10:05 +0900

    빈 생명주기 콜백 - @PostConstruct, @PreDestroy

--- spring-core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring-core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,15 +3,18 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 
 /*
     InitializingBean 은 afterPropertiesSet() 메서드로 초기화를 지원한다.
     DisposableBean 은 destroy() 메서드로 소멸을 지원한다.
 
     초기화, 소멸 인터페이스 단점
-    이 인터페이스는 스프링 전용 인터페이스다. 해당 코드가 스프링 전용 인터페이스에 의존한다.
+    - 이 인터페이스는 스프링 전용 인터페이스다. 해당 코드가 스프링 전용 인터페이스에 의존한다.
     초기화, 소멸 메서드의 이름을 변경할 수 없다.
-    내가 코드를 고칠 수 없는 외부 라이브러리에 적용할 수 없다.
+    - 내가 코드를 고칠 수 없는 외부 라이브러리에 적용할 수 없다.
     -> 옛날거라 사용x
     public class NetworkClient implements InitializingBean, DisposableBean {
 */
@@ -43,11 +46,22 @@ public class NetworkClient {
     }
 
 
+    /*
+        @PostConstruct, @PreDestroy 애노테이션을 사용하자
+        - 코드를 고칠 수 없는 외부 라이브러리를 초기화, 종료해야 하면 @Bean 의 initMethod , destroyMethod
+        를 사용하자.
+        - 최신 스프링에서 가장 권장하는 방법이다.
+        - 애노테이션 하나만 붙이면 되므로 매우 편리하다.
+    */
+
+    @PostConstruct
     public void init() throws Exception {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
+
+    @PreDestroy
     public void close() throws Exception {
         System.out.println("NetworkClient.close");
         disconnect();
