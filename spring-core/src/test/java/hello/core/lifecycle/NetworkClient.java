a8c902e6d2826c4b8274d1c630df97eedd7aa86d
wookyoungK <pd1125@naver.com>
2022-09-13 21:39:15 +0900

    빈 생명주기 콜백 - 초기화, 소멸 인터페이스

--- spring-core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring-core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,7 +1,21 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
-    
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+
+/*
+    InitializingBean 은 afterPropertiesSet() 메서드로 초기화를 지원한다.
+    DisposableBean 은 destroy() 메서드로 소멸을 지원한다.
+
+    초기화, 소멸 인터페이스 단점
+    이 인터페이스는 스프링 전용 인터페이스다. 해당 코드가 스프링 전용 인터페이스에 의존한다.
+    초기화, 소멸 메서드의 이름을 변경할 수 없다.
+    내가 코드를 고칠 수 없는 외부 라이브러리에 적용할 수 없다.
+    -> 옛날거라 사용x
+* */
+public class NetworkClient implements InitializingBean, DisposableBean {
+
     private String url;
 
     public NetworkClient(){
@@ -9,7 +23,7 @@ public class NetworkClient {
         connect();
         call("초기화 연결 메시지");
     }
-    
+
     public void setUrl(String url) {
         this.url = url;
     }
@@ -24,5 +38,16 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
