939ea130e3fed49ba465e64ab574d334d2b8942d
wookyoungK <pd1125@naver.com>
2022-09-27 23:32:04 +0900

    스코프와 프록시

--- spring-core/src/main/java/hello/core/common/MyLogger.java
+++ spring-core/src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,20 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+/*
+- MyLogger의 가짜 프록시 클래스를 만들어두고 HTTP request와 상관 없이 가짜 프록시
+클래스를 다른 빈에 미리 주입해 둘 수 있다
+
+- CGLIB라는 라이브러리로 내 클래스를 상속 받은 가짜 프록시 객체를 만들어서 주입한다.
+- 가짜 프록시 객체는 요청이 오면 그때 내부에서 진짜 빈을 요청하는 위임 로직이 들어있다
+- request scope와는 관계가 없다, 싱글톤 처럼 동작한다.
+
+****
+- Provider를 사용하든, 프록시를 사용하든 핵심은 진짜 객체 조회를 꼭 필요한 시점까지 지연처리 한다는 점
+- 단지 애노테이션 설정 변경만으로 원본 객체를 프록시 객체로 대체할 수 있다. 이것이 바로 다형성과 DI 컨테이너가 가진 큰 강점이다.
+- 꼭 웹 스코프가 아니어도 프록시는 사용할 수 있다.
+*/
 public class MyLogger {
 
     private String uuid;
--- spring-core/src/main/java/hello/core/web/LogDemService.java
+++ spring-core/src/main/java/hello/core/web/LogDemService.java
@@ -9,10 +9,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LogDemService {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
 
     }
+    /*
+    클라이언트가 myLogger.logic() 을 호출하면 사실은 가짜 프록시 객체의 메서드를 호출한 것이다.
+    가짜 프록시 객체는 request 스코프의 진짜 myLogger.logic() 를 호출한다.
+    */
 }
